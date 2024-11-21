9c1f7f91b582716d1350ff708f1b22d81f4fe4f1
n0tx <rcandra91@msn.com>
2024-11-21 15:37:09 +0700

    Initialize Spring Boot Simple Login Authentication

---
+++ src/test/java/com/riki/auth/SimpleLoginAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLoginAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
